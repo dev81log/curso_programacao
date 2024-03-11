09f257f7666ed0f880b60c5806e79838b26f318e
dev81log <dev81log@gmail.com>
2024-03-11 15:33:24 -0300

    Exercicio de fixacao de variaveis

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,41 @@
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá mundo!");
+
+        double variavelX = 10.35784;
+        System.out.println(variavelX);
+        System.out.printf("%.2f%n", variavelX);
+        System.out.printf("%.4f%n", variavelX);
+
+        Locale.setDefault(Locale.US);
+
+        String nome = "Maria";
+        int idade = 31;
+        double renda = 4000.0;
+        System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+        System.out.println();
+
+        String product1 = "Computer";
+        String product2 = "Office desk";
+
+        int age = 30;
+        int code = 5290;
+        char gender = 'F';
+
+        double price1 = 2100.0;
+        double price2 = 650.50;
+        double measure = 53.234567;
+
+        System.out.println("Products:");
+        System.out.printf("%s, which price is $ %.2f%n", product1, price1);
+        System.out.printf("%s, which price is $ %.2f%n", product2, price2);
+        System.out.println();
+        System.out.printf("Record: %d years old, code %d and gender: %c%n", age, code, gender);
+        System.out.println();
+        System.out.printf("Measue with eight decimal places: %.8f%n", measure);
+        System.out.printf("Rouded (three decimal places): %.3f%n", measure);
+        System.out.printf("US decimal point: %.3f%n", measure);
+
     }
 }
