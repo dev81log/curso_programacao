389daf3b6c1828ac0636fcd1cbb06587c5936146
dev81log <dev81log@gmail.com>
2024-03-12 10:04:53 -0300

    Estrutura sequencial - Exercicios terreno

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,26 @@
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        double variavelX = 10.35784;
-        System.out.println(variavelX);
-        System.out.printf("%.2f%n", variavelX);
-        System.out.printf("%.4f%n", variavelX);
+        double largura, comprimento, metroQuadrado, area, preco;
 
-        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
 
-        String nome = "Maria";
-        int idade = 31;
-        double renda = 4000.0;
-        System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+        System.out.println("Digite a largura do terreno: ");
+        largura = scanner.nextDouble();
+        System.out.println("Digite o comprimento do terreno: ");
+        comprimento = scanner.nextDouble();
+        System.out.println("Digite o valor do metro quadrado: ");
+        metroQuadrado = scanner.nextDouble();
 
-        String product1 = "Computer";
-        String product2 = "Office desk";
+        area = largura * comprimento;
+        preco = area * metroQuadrado;
 
-        int age = 30;
-        int code = 5290;
-        char gender = 'F';
+        System.out.printf("Área do terreno: %.2f%n", area);
+        System.out.printf("Preço do terreno: %.2f%n", preco);
 
-        double price1 = 2100.0;
-        double price2 = 650.50;
-        double measure = 53.23456700;
-
-        System.out.printf("Products:%n%s, which price is $ %.2f%n%s, which price is $ %.2f%n", product1, price1, product2, price2);
-        System.out.printf("Record: %d years old, code %d and gender: %c%n", age, code, gender);
-        System.out.printf("Measure with eight decimal places: %.8f%n", measure);
-        System.out.printf("Rouded (three decimal places): %.3f%n", measure);
-        System.out.printf("US decimal point: %.3f%n", measure);
     }
 }
