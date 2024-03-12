1d36661832f3bacb612f687b3d584efe3e3d6d09
dev81log <dev81log@gmail.com>
2024-03-11 21:50:23 -0300

    Exercicio de fixacao de variaveis

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá mundo!");
+
+        double variavelX = 10.35784;
+        System.out.println(variavelX);
+        System.out.printf("%.2f%n", variavelX);
+        System.out.printf("%.4f%n", variavelX);
+
+        Locale.setDefault(Locale.US);
+
+        String nome = "Maria";
+        int idade = 31;
+        double renda = 4000.0;
+        System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
     }
 }
