3ce7486a6a65ff7978b583b9862594369c84587f
dev81log <dev81log@gmail.com>
2024-03-11 21:53:43 -0300

    Exercicio de fixacao de variaveis final

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,22 @@ public class Main {
         int idade = 31;
         double renda = 4000.0;
         System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+
+        String product1 = "Computer";
+        String product2 = "Office desk";
+
+        int age = 30;
+        int code = 5290;
+        char gender = 'F';
+
+        double price1 = 2100.0;
+        double price2 = 650.50;
+        double measure = 53.23456700;
+
+        System.out.printf("Products:%n%s, which price is $ %.2f%n%s, which price is $ %.2f%n", product1, price1, product2, price2);
+        System.out.printf("Record: %d years old, code %d and gender: %c%n", age, code, gender);
+        System.out.printf("Measure with eight decimal places: %.8f%n", measure);
+        System.out.printf("Rouded (three decimal places): %.3f%n", measure);
+        System.out.printf("US decimal point: %.3f%n", measure);
     }
 }
