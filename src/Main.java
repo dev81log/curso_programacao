b59b16981da1f4e5e13351ea5acc983b18c81296
dev81log <dev81log@gmail.com>
2024-03-12 10:15:29 -0300

    Estrutura sequencial - Exercicio idade

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        double largura, comprimento, metroQuadrado, area, preco;
-
         Scanner scanner = new Scanner(System.in);
         scanner.useLocale(Locale.US);
+
+
+        /*
+        // Terreno
+        double largura, comprimento, metroQuadrado, area, preco;
 
         System.out.println("Digite a largura do terreno: ");
         largura = scanner.nextDouble();
@@ -20,7 +23,32 @@ public class Main {
         preco = area * metroQuadrado;
 
         System.out.printf("Área do terreno: %.2f%n", area);
-        System.out.printf("Preço do terreno: %.2f%n", preco);
+        System.out.printf("Preço do terreno: %.2f%n", preco);*/
+
+
+        // Idades
+        String nomeA, nomeB;
+        int idadeA, idadeB;
+        double mediaIdade;
+
+        System.out.println("Dados da primeira pessoa: ");
+        System.out.println("Nome: ");
+        nomeA = scanner.nextLine();
+        System.out.println("Idade: ");
+        idadeA = scanner.nextInt();
+
+        scanner.nextLine();
+
+        System.out.println("Dados da segunda pessoa: ");
+        System.out.println("Nome: ");
+        nomeB = scanner.nextLine();
+        System.out.println("Idade: ");
+        idadeB = scanner.nextInt();
+
+        mediaIdade = (double) (idadeA + idadeB) / 2;
+
+        System.out.printf("A idade média de %s e %s é de %.1f anos%n", nomeA, nomeB, mediaIdade);
+
 
     }
 }
